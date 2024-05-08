7a18bcbbfd9c094a27b2d10a6a79680a44944e77
morry6388 <shangrilasars6388@gmail.com>
2024-05-08 11:26:30 +0900

    add npm package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/layout/Header';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   const [username, setUsername] = useState(null);
 
   useEffect(() => {
-    // バックエンドからユーザー情報を取得するロジックをここに記述
-    // 例: setUsername('ユーザー名');
+    axios.get('http://localhost:8080/api/user')
+      .then(response => {
+        setUsername(response.data);
+      })
+      .catch(error => console.error('Error fetching user data:', error));
   }, []);
 
   return (
